8724f5b54dd9bfb3997be45d45111d135bcc3aec
kaiross12 <yaserdurubi1209@gmail.com>
2026-08-07 00:47:30 +0300

    Merges

--- src/components/WhatWeDo.vue
+++ src/components/WhatWeDo.vue
@@ -298,6 +298,8 @@ onUnmounted(() => {
   -webkit-mask:
     linear-gradient(#fff 0 0) content-box,
     linear-gradient(#fff 0 0);
+    mask: linear-gradient(#fff 0 0) content-box,
+    linear-gradient(#fff 0 0);
   mask: linear-gradient(#fff 0 0) content-box,
     linear-gradient(#fff 0 0);
   -webkit-mask-composite: xor;
